a3d3993d70edbb9ac7d9cbf118f3f967b8700768
Allison Cole <allisonleahcole@gmail.com>
2020-06-23 11:09:34 -0400

    fav number

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace SelectionStatements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the random number guessing game!");
+            Console.WriteLine("See if you can guess the correct number!");
+            Console.WriteLine("What is the upper limit for the range of the number?");
+
+            var userResponse = Console.ReadLine();
+
+            var upperLimit = int.Parse(userResponse);
+
+            var random = new Random();
+            var number = random.Next(1, upperLimit);
+
+            Console.WriteLine("Input your guess:");
+
+            var guess = int.Parse(Console.ReadLine());
+
+            if(guess < number)
+            {
+                Console.WriteLine("too low!");
+            }
+            else if(guess > number)
+            {
+                Console.WriteLine("too high");
+            }
+            else
+            {
+                Console.WriteLine("Nevermind!");
+            }
+        }
+    }
+}
